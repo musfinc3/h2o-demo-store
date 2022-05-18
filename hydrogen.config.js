56280dc124627d4473eb111ce994751c3c765f6d
Md Musfiqur Rahman <105712245+musfinc3@users.noreply.github.com>
2022-05-18 12:58:25 +0200

    Add files via upload

---
+++ hydrogen.config.js
@@ -0,0 +1,22 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+import {
+  CookieSessionStorage,
+  PerformanceMetricsServerAnalyticsConnector,
+} from '@shopify/hydrogen';
+
+export default defineConfig({
+  routes: import.meta.globEager('./src/routes/**/*.server.[jt](s|sx)'),
+  shopify: {
+    storeDomain: 'https://h2o-demo-store.myshopify.com',
+    storefrontToken: '45f2546d61a0a36280066948313e8094',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
+});
